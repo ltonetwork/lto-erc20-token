32f89681ceae244cc4212c7afb4d78df50e90bff
Bruno Brito <brunodb3@gmail.com>
2021-10-24 15:32:30 +0200

    Fixing bugs and separating approve from swap calls

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,51 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { WalletService } from '@app/services';
+import { Tokens, WalletService } from '@app/services';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   defaultErrorMsg = 'Something bad happened, please try again.';
-  tokens!: {
-    v1: string;
-    v2: string;
-  };
+  tokens$!: Subscription;
 
   public loading: boolean = false;
-  public swapDisabled: boolean = false;
+  public tokens: Tokens | undefined;
 
   constructor(
     private walletService: WalletService,
     private _snackBar: MatSnackBar
-  ) {}
-
-  ngOnInit() {
-    this.getBalance();
+  ) {
+    this.loading = true;
   }
 
-  async getBalance() {
+  ngOnInit() {
+    this.tokens$ = this.walletService.tokens$.subscribe(
+      (tokens) => {
+        this.tokens = tokens;
+
+        if (this.tokens) {
+          this.loading = false;
+
+          if (this.tokens.v1.raw <= 0) {
+            this._snackBar.open(
+              `You don't have enough v1 tokens to make a swap`,
+              'Dismiss',
+              {
+                duration: 5000,
+              }
+            );
+          }
+        }
+      },
+      (error) => {
+        this.loading = false;
+        this._snackBar.open(error.message || this.defaultErrorMsg, 'Dismiss', {
+          duration: 5000,
+        });
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.tokens$.unsubscribe();
+  }
+
+  async approve() {
+    if (!this.tokens) return;
     this.loading = true;
-    this.swapDisabled = true;
 
     try {
-      this.tokens = await this.walletService.getTokensBalance();
-      this.loading = false;
-
-      if (this.tokens.v1 === '0.00') {
-        this._snackBar.open(
-          `You don't have enough v1 tokens to make a swap`,
-          'Dismiss',
-          {
-            duration: 5000,
-          }
-        );
-
-        return;
-      }
+      await this.walletService.tokenApprove(100000000);
     } catch (error: any) {
       this._snackBar.open(error.message || this.defaultErrorMsg, 'Dismiss', {
         duration: 5000,
@@ -53,29 +68,26 @@ export class HomeComponent implements OnInit {
     }
 
     this.loading = false;
-    this.swapDisabled = false;
   }
 
   async swap() {
-    if (this.tokens.v1 === '0.00') {
+    if (!this.tokens) return;
+
+    if (this.tokens.allowance.raw <= 0) {
       this._snackBar.open(
-        `You don't have enough v1 tokens to make a swap`,
+        `Your allowance is not enough to make a swap`,
         'Dismiss',
         {
           duration: 5000,
         }
       );
-
       return;
     }
 
     this.loading = true;
-    this.swapDisabled = true;
 
     try {
       await this.walletService.tokenSwap();
-
-      await this.getBalance();
     } catch (error: any) {
       this._snackBar.open(error.message || this.defaultErrorMsg, 'Dismiss', {
         duration: 5000,
@@ -83,6 +95,5 @@ export class HomeComponent implements OnInit {
     }
 
     this.loading = false;
-    this.swapDisabled = false;
   }
 }
